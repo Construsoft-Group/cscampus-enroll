263f5e79093f8c889651b435a406198335db8051
Juan Díaz <juafdiazdam@gmail.com>
2025-10-24 15:26:31 +0200

    Se incluyen nuevos productos hotmart y sus cursos

--- src/config/hotmartProducts.js
+++ src/config/hotmartProducts.js
@@ -39,6 +39,76 @@ const hotmartPacks = [{
         ],
         "matricula": 60
     },
+    "Componentes Personalizados en Tekla Structures": {
+        "courses": [
+        {
+            "name": "Componentes Personalizados en Tekla Structures",
+            "url": "https://campus.construsoft.com/course/view.php?id=89",
+            "id": 89
+        }
+        ],
+        "matricula": 60
+    },
+    "Editor de Cuadros en Tekla Structures": {
+        "courses": [
+        {
+            "name": "Editor de Cuadros en Tekla Structures",
+            "url": "https://campus.construsoft.com/course/view.php?id=113",
+            "id": 113
+        }
+        ],
+        "matricula": 60
+    },
+    "Gestión de la Numeración de Tekla Structures": {
+        "courses": [
+        {
+            "name": "Gestión de la Numeración de Tekla Structures",
+            "url": "https://campus.construsoft.com/course/view.php?id=114",
+            "id": 114
+        }
+        ],
+        "matricula": 60
+    },
+    "Interoperabilidad con Tekla Structures": {
+        "courses": [
+        {
+            "name": "Interoperabilidad con Tekla Structures",
+            "url": "https://campus.construsoft.com/course/view.php?id=144",
+            "id": 144
+        }
+        ],
+        "matricula": 60
+    },
+    "Dibujos avanzados en Tekla Structures Acero": {
+        "courses": [
+        {
+            "name": "Dibujos avanzados en Tekla Structures Acero",
+            "url": "https://campus.construsoft.com/course/view.php?id=212",
+            "id": 212
+        }
+        ],
+        "matricula": 60
+    },
+    "Dibujos avanzados en Tekla Structures Hormigón": {
+        "courses": [
+        {
+            "name": "Dibujos avanzados en Tekla Structures Hormigón",
+            "url": "https://campus.construsoft.com/course/view.php?id=161",
+            "id": 161
+        }
+        ],
+        "matricula": 60
+    },
+    "Detallado de Elementos Prefabricados en Tekla Structures": {
+        "courses": [
+        {
+            "name": "Detallado de Elementos Prefabricados en Tekla Structures",
+            "url": "https://campus.construsoft.com/course/view.php?id=116",
+            "id": 116
+        }
+        ],
+        "matricula": 60
+    },
     "Teoría y Cálculo de Elementos de HA pretensado, postensado y regiones D con IDEA StatiCa": {
         "courses": [
         {
@@ -273,6 +343,66 @@ const hotmartPacks = [{
         }
         ],
         "matricula": 600
+    },
+    "Pack BIM | Total Acero": {
+        "courses": [
+        {
+            "name": "Fundamentos Tekla Acero",
+            "url": "https://campus.construsoft.com/course/view.php?id=149",
+            "id": 149
+        },
+        {
+            "name": "Análisis y Diseño de Edificaciones con Tekla Structural Designer",
+            "url": "https://campus.construsoft.com/course/view.php?id=135",
+            "id": 135
+        },
+        {
+            "name": "Teoría y cálculo de uniones metálicas con IDEA StatiCa",
+            "url": "https://campus.construsoft.com/course/view.php?id=76",
+            "id": 76
+        },
+        {
+            "name": "Cálculo de resistencia al fuego y protección pasiva",
+            "url": "https://campus.construsoft.com/course/view.php?id=73",
+            "id": 73
+        },
+        {
+            "name": "CDE | Gestión y coordinación de proyectos BIM con Trimble Connect",
+            "url": "https://campus.construsoft.com/course/view.php?id=174",
+            "id": 174
+        }
+        ],
+        "matricula": 400
+    },
+    "Pack BIM | Total Hormigón": {
+        "courses": [
+        {
+            "name": "Fundamentos Tekla Hormigón",
+            "url": "https://campus.construsoft.com/course/view.php?id=195",
+            "id": 195
+        },
+        {
+            "name": "Análisis y Diseño de Edificaciones con Tekla Structural Designer",
+            "url": "https://campus.construsoft.com/course/view.php?id=135",
+            "id": 135
+        },
+        {
+            "name": "Teoría y Cálculo de Elementos de HA pretensado, postensado y regiones D con IDEA StatiCa",
+            "url": "https://campus.construsoft.com/course/view.php?id=141",
+            "id": 141
+        },
+        {
+            "name": "Cálculo de resistencia al fuego y protección pasiva",
+            "url": "https://campus.construsoft.com/course/view.php?id=73",
+            "id": 73
+        },
+        {
+            "name": "CDE | Gestión y coordinación de proyectos BIM con Trimble Connect",
+            "url": "https://campus.construsoft.com/course/view.php?id=174",
+            "id": 174
+        }
+        ],
+        "matricula": 400
     }
 }];
 
